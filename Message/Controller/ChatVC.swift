864867548f5c2ea5dfde1b1c97d4bc2cb90a47ab
Mohsen Abdollahi <mohsen9april@gmail.com>
2019-07-06 20:42:50 +0430

    send Message by all Content ( email , message body  , sender & username & userprofileLink )

--- Message/Controller/ChatVC.swift
+++ Message/Controller/ChatVC.swift
@@ -16,6 +16,8 @@ class ChatVC: UIViewController, UITextFieldDelegate , UITableViewDelegate , UITa
     
     var messageArray = [Message]()
     var userContenArray = [UserContent]()
+    var count = 0
+    
     
     
     @IBOutlet weak var chatTableView: UITableView!
@@ -83,15 +85,26 @@ class ChatVC: UIViewController, UITextFieldDelegate , UITableViewDelegate , UITa
     
     //Save Messages to Database
     @IBAction func sendButtonClicked(_ sender: Any) {
-        
         //First Disable Button and Text for
         messageTextField.isEnabled = false
         sendBtn.isEnabled = false
        
         guard let sender = Auth.auth().currentUser?.email else { return }
         guard let messageBody = messageTextField.text else { return }
-        let messageDictionary = ["sender" : sender , "messageBody" : messageBody ]
-        
+        // send some Extra Data to save in Database Message
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        print("uid Current user is : ******************")
+        print(uid)
+        Database.database().reference().child("users").child(uid).observe(DataEventType.value) { (Datasnapshot) in
+            let snapshot = Datasnapshot.value as! [String: String]
+            guard let email = snapshot["email"] else { return }
+            guard let username = snapshot["username"] else { return }
+            guard let usersImageProfileLink = snapshot["usersImageProfile"] else { return }
+        let messageDictionary = ["sender" : sender ,
+                                 "messageBody" : messageBody,
+                                 "email" : email,
+                                 "username" : username,
+                                 "usersImageProfileLink" : usersImageProfileLink ]
         let MessageDB = Database.database().reference().child("Messages")
         MessageDB.childByAutoId().setValue(messageDictionary) { (Error, Reference ) in
             if let error = Error {
@@ -104,29 +117,9 @@ class ChatVC: UIViewController, UITextFieldDelegate , UITableViewDelegate , UITa
             self.messageTextField.isEnabled = true
             self.sendBtn.isEnabled = true
             self.messageTextField.text = ""
-        }
-    }
-    var count = 0
-    func retriveMessages(){
-        
-        //Fetch Data from Message Database ( sender & MessageBody)
-        let messageDB = Database.database().reference().child("Messages")
-        messageDB.observe(.childAdded) { (DataSnapshot) in
-            let snapshot = DataSnapshot.value as! [String: String]
-            guard let  sender = snapshot["sender"] else { return }
-            guard let  messageBody = snapshot["messageBody"] else { return }
-            let message = Message(sender: sender, messageBody: messageBody)
-            
-            
-            self.count = self.count + 1
-            print(self.count)
-            
-            
-            self.messageArray.append(message)
-            self.chatTableView.reloadData()
-
-        }
-    }
+          }
+      }
+  }
     
     func fetchUserContent(){
         guard let uid = Auth.auth().currentUser?.uid else { return }
@@ -140,11 +133,31 @@ class ChatVC: UIViewController, UITextFieldDelegate , UITableViewDelegate , UITa
             guard let usersImageProfileLink = snapshot["usersImageProfile"] else { return }
             let usercontent = UserContent(email: email, username: username, userImageProfileLink: usersImageProfileLink)
             self.userContenArray.append(usercontent)
-
+            
+            print("User Content is : ")
+            print(usercontent.email)
+            print(usercontent.username)
+            print(usercontent.userImageProfileLink)
         }
     }
-    
-    
+
+    func retriveMessages(){
+        //Fetch Data from Message Database ( sender & MessageBody)
+        let messageDB = Database.database().reference().child("Messages")
+        messageDB.observe(.childAdded) { (DataSnapshot) in
+            let snapshot = DataSnapshot.value as! [String: String]
+            guard let  sender = snapshot["sender"] else { return }
+            guard let  messageBody = snapshot["messageBody"] else { return }
+            let message = Message(sender: sender, messageBody: messageBody)
+
+            self.count = self.count + 1
+            print(self.count)
+
+            self.messageArray.append(message)
+            self.chatTableView.reloadData()
+        }
+    }
+
     //Configure TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return list.count
